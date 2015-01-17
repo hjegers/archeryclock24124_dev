d9847ac6657ead5316ea15277a2b5bf2663da013
Henk Jegers <henk.jegers@gmail.com>
2015-01-17 14:13:41 +0100

    Update Countdown.pas

--- archeryclock24124_dev/Countdown.pas
+++ archeryclock24124_dev/Countdown.pas
@@ -1,7 +1,7 @@
 unit Countdown;
 
 {*
-Copyright (C) 2010-2014  Henk Jegers
+Copyright (C) 2010-2015  Henk Jegers
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
@@ -15,7 +15,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 for questions email to info@archeryclock.com
 
-this software uses SDPO serial, SDL and MMSystem
+this software uses SDPO serial, SDL, LNET and MMSystem
 
 Contributions: Yann Morere (Linux)
                Henk Jegers (initial version/Windows.)
